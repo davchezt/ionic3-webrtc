9110dd933a51af8dfc9f0f67c2f1bb260dfdfe24
Raiza Rhamdan a.k.a Leonardo DaVchezt <davchezt@gmail.com>
2019-02-04 17:38:55 +0700

    uyeh

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
-import { AndroidPermissions } from '@ionic-native/android-permissions';
-import { BackgroundMode } from '@ionic-native/background-mode';
+// import { AndroidPermissions } from '@ionic-native/android-permissions';
+// import { BackgroundMode } from '@ionic-native/background-mode';
 
 import { Socket } from 'ng-socket-io';
 
@@ -14,6 +14,7 @@ declare var SimplePeer;
 
 export class HomePage {
   @ViewChild('rtcVideo') rtcVideo;
+  @ViewChild('locVideo') locVideo;
 
   targetpeer: any;
   peer: any;
@@ -21,107 +22,116 @@ export class HomePage {
   caller: boolean = false;
   isConnected: boolean = false;
   isInit: boolean = false;
+  roomId = "vidoe-call";
   constructor(
     public navCtrl: NavController,
     public socket: Socket, 
-    private androidPermissions: AndroidPermissions,
-    private platform: Platform,
-    private backgroundMode: BackgroundMode
+    // private androidPermissions: AndroidPermissions,
+    // private platform: Platform,
+    // private backgroundMode: BackgroundMode
   ) {
-    if (this.backgroundMode.isEnabled() && this.platform.is('android')) {
-      if (this.backgroundMode.isActive()) {
-        if (!this.isInit) this.startAnu();
-      }
-      if (this.platform.is('android')) {
-        try {
-          this.backgroundMode.on("activate").subscribe(()=>{
-            this.backgroundMode.disableWebViewOptimizations();
-            this.backgroundMode.moveToBackground();
-            this.socket.emit('ended-video');
-          });
-        }
-        catch(err) {
-          console.log(err);
-        }
-      }
-    }
-    else {
-      if (!this.isInit) this.startAnu();
-    }
+    // if (!this.isInit) 
+    // this.startAnu();
+    // if (this.backgroundMode.isEnabled() && this.platform.is('android')) {
+    //   if (this.backgroundMode.isActive()) {
+    //     if (!this.isInit) this.startAnu();
+    //   }
+    //   if (this.platform.is('android')) {
+    //     try {
+    //       this.backgroundMode.on("activate").subscribe(()=>{
+    //         this.backgroundMode.disableWebViewOptimizations();
+    //         this.backgroundMode.moveToBackground();
+    //         this.socket.emit('stop-call');
+    //       });
+    //     }
+    //     catch(err) {
+    //       console.log(err);
+    //     }
+    //   }
+    // }
     // Detect run on background
-    window.addEventListener('pause', () => {
-      console.log('app:pause');
-      this.socket.emit('ended-video');
-    }, false);
-    window.addEventListener('resume', () => {
-      console.log("app:resume");
-      this.socket.emit('ended-video');
-    }, false);
+    // window.addEventListener('pause', () => {
+    //   console.log('app:pause');
+    //   this.socket.emit('stop-call');
+    // }, false);
+    // window.addEventListener('resume', () => {
+    //   console.log("app:resume");
+    //   this.socket.emit('stop-call');
+    // }, false);
     
   }
 
   ionViewDidLoad() {
-    console.log(this.rtcVideo)
+    // console.log(this.rtcVideo)
+    this.startAnu();
   }
 
   startAnu() {
     this.caller = location.hash === '#call';
-    let that = this;
     this.socket.connect();
-    this.socket.on('call-video', (data) => {
+    this.socket.emit('subscribe', this.roomId);
+    this.socket.on('start-call', (data) => {
       let peer:any = data;
-      if (peer.type === 'offer') {
+      if (peer.data.type === 'offer') {
         if (location.hash !== '#call') {
-          console.log('offer:', data);
-          that.targetpeer = JSON.stringify(data);
+          console.log('offer:', peer.data);
+          this.targetpeer = JSON.stringify(peer.data);
         }
       }
       else {
         if (location.hash === '#call') {
-          console.log('answer:', data);
-          that.targetpeer = JSON.stringify(data);
-          if (this.peer) this.peer.signal(JSON.parse(this.targetpeer));
+          console.log('answer:', peer.data);
+          this.targetpeer = JSON.stringify(peer.data);
+          if (this.peer) {
+            this.peer.signal(JSON.parse(this.targetpeer));
+          }
         }
       }
     })
-    this.socket.on('incall-video', () => {
+    this.socket.on('in-call', (data) => {
       this.isConnected = true;
     });
-    this.socket.on('ended-video', () => {
+    this.socket.on('chat-call', (data) => {
+      let peer:any = data;
+      console.log(peer.message);
+    });
+    this.socket.on('stop-call', (data) => {
       this.isConnected = false;
     })
-    if (this.platform.is('android')) {
-      this.checkPermissions();
-    }
+    // if (this.platform.is('android')) {
+    //   this.checkPermissions();
+    // }
     this.init();
-    this.isInit = true;
+    // this.isInit = true;
   }
 
-  checkPermissions(){
-    this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA, this.androidPermissions.PERMISSION.RECORD_AUDIO]);
+  // checkPermissions(){
+  //   this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA, this.androidPermissions.PERMISSION.RECORD_AUDIO]);
 
-    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
-      success => console.log("Hey you have permission"),
-      err => {
-        console.log("Uh oh, looks like you don't have permission");
-        this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA);
-      }
-    );
+  //   this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
+  //     success => console.log("Hey you have permission"),
+  //     err => {
+  //       console.log("Uh oh, looks like you don't have permission");
+  //       this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA);
+  //     }
+  //   );
 
-    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.RECORD_AUDIO).then(
-      success => console.log("Hey you have permission"),
-      err => {
-        console.log("Uh oh, looks like you don't have permission");
-        this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.RECORD_AUDIO);
-      }
-    );
-  }
+  //   this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.RECORD_AUDIO).then(
+  //     success => console.log("Hey you have permission"),
+  //     err => {
+  //       console.log("Uh oh, looks like you don't have permission");
+  //       this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.RECORD_AUDIO);
+  //     }
+  //   );
+  // }
 
   init() {
-    let peerx: any;
+    let peerx: any = null;
     let that = this;
     this.n.getUserMedia = (this.n.getUserMedia || this.n.webkitGetUserMedia || this.n.mozGetUserMedia || this.n.msGetUserMedia);
     this.n.getUserMedia({ video:true, audio:true }, function(stream) {
+      that.locVideo.nativeElement.srcObject = stream;
+      that.locVideo.nativeElement.play();
       peerx = new SimplePeer({
         initiator: location.hash === '#call',
         trickle: false,
@@ -133,38 +143,37 @@ export class HomePage {
       })
 
       peerx.on('connect', function() {
-        that.socket.emit('incall-video');
+        that.socket.emit('in-call', { room: that.roomId });
         console.log("peer connect");
         console.log("isConnected: ", that.isConnected)
       })
       
       peerx.on('signal', function(data) {
-        console.log('signal:', JSON.stringify(data));
+        // console.log('signal:', JSON.stringify(data));
+        // that.locVideo.nativeElement.srcObject = stream;
+        // that.locVideo.nativeElement.play();
         
         if (location.hash !== '#call') {
           that.targetpeer = JSON.stringify(data);
         }
-        that.socket.emit('call-video', data);
+        that.socket.emit('start-call', { room: that.roomId, data: data });
       })
       
       peerx.on('data', function(data) {
         console.log('Recieved message:' + data);
       })
       
-      peerx.on('stream', function(stream) {
-        console.log(that.rtcVideo);
+      peerx.on('stream', function(streams) {
+        // console.log(that.rtcVideo);
         // that.rtcVideo.nativeElement.src = URL.createObjectURL(stream);
-        that.rtcVideo.nativeElement.srcObject = stream;
+        that.rtcVideo.nativeElement.srcObject = streams;
         that.rtcVideo.nativeElement.play();
       })
 
       peerx.on('close', function() {
-        that.rtcVideo.nativeElement.srcObject = null;
-        that.rtcVideo.nativeElement.pause();
         console.log("peer close");
-        that.targetpeer = null;
-        that.isConnected = false;
-        that.socket.emit('ended-video');
+        that.disconnect();
+        that.socket.emit('stop-call', { room: that.roomId });
         // that.init();
       })
       peerx.on('error', function(err) {
@@ -174,13 +183,14 @@ export class HomePage {
       })
     
     }, function(err){
-    console.log('Failed to get stream', err);
+      console.log('Failed to get stream', err);
     });
     
+    const timeOut = peerx !== null ? 100:5000;
     setTimeout(() => {
       this.peer = peerx;
-      console.log('peer:constructor', JSON.stringify(this.peer));
-    }, 5000);
+      console.log('peer:constructor', this.peer);
+    }, timeOut);
   }
 
   connect() {
@@ -190,11 +200,25 @@ export class HomePage {
   }
   
   message() {
-    if (this.peer) this.peer.send('Hello world');
+    if (this.peer) {
+      this.peer.send("test");
+      this.socket.emit('chat-call', { room: this.roomId, message: "test"});
+    }
   }
 
   disconnect() {
-    if (this.peer) this.peer.destroy();
+    if (this.peer) {
+      this.rtcVideo.nativeElement.pause();
+      this.rtcVideo.nativeElement.srcObject = null;
+
+      this.peer.destroy();
+      this.peer = null;
+      this.targetpeer = null;
+      const timeOut = this.peer !== null ? 100:5000;
+      setTimeout(() => {
+        this.init();
+      }, timeOut);
+    }
   }
 
 }
